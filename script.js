f06562a7b63de43df1398d77905a5198882c4bcf
Dinesh_Reddy <119753519+Dinesh-7075@users.noreply.github.com>
2023-06-05 21:50:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your code here
 function swapTheme() {
    var element = document.getElementById("app");
-  element.classList.toggle("day");
+  element.classList.toggle("night");
 	
 	var element = document.getElementById("swap");
   element.classList.toggle("button_night");
