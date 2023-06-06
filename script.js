611a9c5eaf948842824cf97382dfee442cdf3f92
Dinesh_Reddy <119753519+Dinesh-7075@users.noreply.github.com>
2023-06-06 12:05:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function swapTheme() {
    var element = document.getElementById("app");
   element.classList.toggle("day");
 	
-	var element = document.getElementById("swap");
-  element.classList.toggle("button_day");
+	var element1 = document.getElementById("swap");
+  element1.classList.toggle("button_day");
 	
 }
